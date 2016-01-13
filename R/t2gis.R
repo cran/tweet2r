3c2e764bf072a30744d587e0b029eb3481ce56ac
Pau Aragó <parago@uji.es>
2016-01-13 22:37:32 +0000

    version 0.4.1

--- R/t2gis.R
+++ R/t2gis.R
@@ -23,26 +23,32 @@ con <- dbConnect(SQLite(), dbname)
                                                 proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")))
          
          #write output message
-         message(paste("Imported files as", geotable, sep=" "))
+         message(paste("Imported files as", geotable, "SpatialPointDataFrame", sep=" "))
 
   switch(export,
          #case export as shp
          shp={
-           writeOGR(eval(parse(text=geotable)), "." , "geotable", driver="ESRI Shapefile")
+           writeOGR(eval(parse(text=geotable)), "." , geotable, driver="ESRI Shapefile")
            
-           msg2=paste("exported file as ", geotable, ".shp", sep="" )
+           msg2=paste("exported file as ", geotable, ".shp in folder", getwd(), sep="" )
+
          },
          #case export as a gml
          gml={
            writeOGR(eval(parse(text=geotable)),paste(geotable, ".gml", sep="") ,"geotable", driver="GML")
            
-           msg2=paste("exported fils as ", geotable, ".gml", sep="" )
+
+           msg2=paste("exported fils as ", geotable, ".gml in folder", getwd(), sep="" )
+
          },
          #case export as a kml
          kml={
            writeOGR(eval(parse(text=geotable)),paste(geotable, ".kml", sep="") ,"geotable", driver="KML")
            
-           msg2=paste("exported file as ", geotable, ".kml", sep="" )
+
+           msg2=paste("exported file as ", geotable, ".kml in folder", getwd(), sep="" )
+
+
          },
          no={
            msg2="no exported file"
